de10b1291b707996447aba6b03d60dff6722c356
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-03-19 21:25:29 +0300

    Initialized app ,api, index route & skeleton for the database

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+from models.models import db
+
+# Initialize app
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///app.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialize API
+api = Api(app)
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+# Index Route
+class Index(Resource):
+
+    def get(self):
+        return '<h1>Hospital Mgmt API Index Page</h1>'
+
+api.add_resource(Index, '/')
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
---
+++ backend/models/models.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+# Models
+
+# Doctor
+class Doctor(db.Model, SerializerMixin):
+    __tablename__ = 'doctors'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+
+# Patient
+class Patient(db.Model, SerializerMixin):
+    __tablename__ = 'patients'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+
+# Doctor_Patient
+class DoctorPatient(db.Model, SerializerMixin):
+    __tablename__ = 'doctor_patients'
+
+    id = db.Column(db.Integer, primary_key=True)
