391e74f268869c0dfd65bdbc89a5a7c1f4f67a1d
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-03-20 21:47:09 +0300

    initial commit

---
+++ backend/models/models.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+# Models
+
+
+# Doctor
+class Doctor(db.Model, SerializerMixin):
+    __tablename__ = 'doctors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String,unique=True, nullable=False)
+    specialty = db.Column(db.String)
+
+
+# Patient
+class Patient(db.Model, SerializerMixin):
+    __tablename__ = 'patients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String,nullable=False)
+    age = db.Column(db.Integer)
+    disease=db.Column(db.String)
+    
+# Doctor_Patient
+class DoctorPatient(db.Model, SerializerMixin):
+    __tablename__ = 'doctor_patients'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+
+# Appointments
+class Appointments(db.Model, SerializerMixin):
+    __tablename__ = 'appointments'
+
+    id = db.Column(db.Integer, primary_key=True)
