e198831b47ff8ab814a8c3ba080ce45e534388de
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-03-25 20:54:43 +0300

    Installed tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hospital Management System</h1>
+    <div className="">
+      <h1 className='font-bold text-2xl text-center'>Hospital Management System</h1>
     </div>
   );
 }
 
 export default App;
+
+
+// MVPs
+
+// Doctor/Patient Login
+// Appointment booking - Patient
+// Appointment management (deleting etc) - Doctor
