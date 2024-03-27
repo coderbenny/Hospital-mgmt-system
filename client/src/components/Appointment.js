b65d2310ee53826b7e1ba8f73ecb66301d8a1f5c
NICKMUGO <nicknjuru13@gmail.com>
2024-03-27 11:24:17 +0300

    Added Routes,PatientView Styling,NotFoundPage,  BookAppointments Page and more pages

---
+++ client/src/components/Appointment.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Appointment() {
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Appointment
--- client/src/components/PatientsView.js
+++ client/src/components/PatientsView.js
@@ -1,29 +1,25 @@
-import React, { useState } from "react"
-import "./PatientsView.css"
+import React, { useState } from "react";
+import "./PatientsView.css";
 
-function PatientsView(){
-    
-    // Appointments state
-    const[appointments, setAppointments]=useState([])
+function PatientsView() {
+  // Appointments state
+  const [appointments, setAppointments] = useState([]);
 
-    return(
-        <div className="bg-cover bg-center h-screen" style={{backgroundImage: `url('/PatientsBG.jpg')`}}>
-            <h1 className="text-center font-bold text-white">Patient View</h1>
-            <div className="flex justify-center items-center ">
-                <button className="bg-green-600 text-center justify-center text-white w-24 h-24 rounded-full items-center">Book Appointments</button>
-                <button className="bg-green-600 text-center justify-center text-white w-24 h-24 rounded-full items-center">Log Out</button>
-                <button className="bg-green-600 text-center justify-center text-white w-24 h-24 rounded-full items-center" >My Profile</button>
-                <button className="bg-green-600 text-center justify-center text-white w-24 h-24 rounded-full items-center">View Appointments</button>
-                <button className="bg-green-600 text-center justify-center text-white w-24 h-24 rounded-full items-center">Book Appointments</button>
-            </div>
+  return (
+    <div className="bg-cover bg-center h-screen" style={{backgroundImage: `url('/PatientsBG.jpg')`}}>
+      <h1 className="text-center font-bold text-white">Patient View</h1>
+      <div className="flex justify-center items-center h-full">
+        <div className="circle">
+          <button className="bg-green-600 text-center text-white w-24 h-24 rounded-full mb-6" onClick={()=>window.location.href="/bookappointments"}>Book Appointments</button>
+          <button className="bg-green-600 text-center text-white w-24 h-24 rounded-full mb-6" onClick={()=>window.location.href="/"}>Log Out</button>
+          <button className="bg-green-600 text-center text-white w-24 h-24 rounded-full mb-6" onClick={()=>window.location.href="/patientsprofile"}>My Profile</button>
+          <button className="bg-green-600 text-center text-white w-24 h-24 rounded-full mb-6" onClick={()=>window.location.href="/patientappointments"}>View Appointments</button>
+          <button className="bg-green-600 text-center text-white w-24 h-24 rounded-full mb-6" onClick={()=>window.location.href="/"}>Book Appointments</button>
+          <button className="bg-green-600 text-center text-white w-24 h-24 rounded-full mb-6" onClick={()=>window.location.href="/"}>New button</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default PatientsView;
-
-// Display list of Doctors and their appointments
-// Button that Routes to booking appointment
-// Button that Routes to updating profile
-// Button for log out
-
