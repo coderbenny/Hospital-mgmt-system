d672013560c0a19cf00854248729bd0e204f255b
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-03-27 22:46:20 +0300

    installed tailwindcss, axios, react-router-dom, flask-cors & flask-session

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hospital Management System</h1>
+    <div className=''>
+        <BrowserRouter>
+            <h1 className='text-center font-bold text-2xl tracking-wide'>Hospital Management System</h1>
+            <Routes>
+                <Route path="/doctor_view"></Route>
+                <Route path="/doctor_registration"></Route>
+                <Route path="/doctor_update"></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
