97edf8a00ef2c685be6b107577fb633f68b6eded
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-03-28 20:18:36 +0300

    intergrated react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
 // import './App.css';
 // import Login from "./Login"
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "./components/Admin";
 
-function App() {
+// function App() {
 
+//   return (
+
+//   );
+// }
+
+export default function App() {
   return (
     <div className="App">
       <h1>Hospital Management System</h1>
-      <Admin />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="admin" element={<Admin />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
