ee9d13ea695f95c49dacfe4ec4d3fdcecd9067c5
ErMuthii <er.muthii@gmail.com>
2024-03-28 19:41:46 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
+import {  BrowserRouter, Route,Routes } from 'react-router-dom';
+import Home from './components/Home';
 import DoctorLogin from './components/DoctorLogin';
 import PatientLogin from './components/PatientLogin';
 import AdminLogin from './components/AdminLogin';
-import {  BrowserRouter, Route,Routes } from 'react-router-dom';
 import RegisterPatient from './components/RegisterPatient';
-import Home from './components/Home';
+import PatientsView from './components/PatientsView'
+import BookAppointments from './components/BookAppointments'
+import PatientProfile from './components/PatientProfile'
+import NotFound from './components/NotFound'
+import Appointment from './components/Appointment'
 
 
 function App() {
   return (
     <div className="App">
-
       <h1 className='font-bold text-2xl text-center color-green'>Hospital Management System</h1>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='doctor_login' element={<DoctorLogin/>}></Route>
-            <Route path='patient_login' element={<PatientLogin/>}></Route>
-            <Route path='admin_login' element={<AdminLogin/>}></Route>
-            <Route path='register_patient' element={<RegisterPatient/>}></Route>
-          </Routes>
-          </BrowserRouter>
-       
+      <BrowserRouter>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='doctor_login' element={<DoctorLogin/>}></Route>
+        <Route path='patient_login' element={<PatientLogin/>}></Route>
+        <Route path='admin_login' element={<AdminLogin/>}></Route>
+        <Route path='register_patient' element={<RegisterPatient/>}></Route>
+        <Route path="/patientview" element={<PatientsView />}/>
+        <Route path="/bookappointments" element={<BookAppointments/>}/>
+        <Route path="/patientsprofile" element={<PatientProfile/>}/>
+        <Route path="/patientappointments" element={<Appointment/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import PatientsView from './components/PatientsView'
-import BookAppointments from './components/BookAppointments'
-import  App from  './App'
-import PatientProfile from './components/PatientProfile'
-import NotFound from './components/NotFound'
-import Appointment from './components/Appointment'
-function NavPatients() {
-  return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element ={<App/>}></Route>
-        <Route path="/patientview" element={<PatientsView />}/>
-        <Route path="/bookappointments" element={<BookAppointments/>}/>
-        <Route path="/patientsprofile" element={<PatientProfile/>}/>
-        <Route path="/patientappointments" element={<Appointment/>}/>
-        <Route path="*" element={<NotFound/>}/>
-    </Routes>
+// import React from 'react'
+// import {BrowserRouter,Routes,Route} from "react-router-dom"
+// import PatientsView from './components/PatientsView'
+// import BookAppointments from './components/BookAppointments'
+// import  App from  './App'
+// import PatientProfile from './components/PatientProfile'
+// import NotFound from './components/NotFound'
+// import Appointment from './components/Appointment'
+// function NavPatients() {
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//         <Route path="/" element ={<App/>}></Route>
+//         <Route path="/patientview" element={<PatientsView />}/>
+//         <Route path="/bookappointments" element={<BookAppointments/>}/>
+//         <Route path="/patientsprofile" element={<PatientProfile/>}/>
+//         <Route path="/patientappointments" element={<Appointment/>}/>
+//         <Route path="*" element={<NotFound/>}/>
+//     </Routes>
       
-    </BrowserRouter>
-  )
-}
+//     </BrowserRouter>
+//   )
+// }
 
-export default NavPatients
+// export default NavPatients
