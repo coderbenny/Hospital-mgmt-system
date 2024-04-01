d6101715105b3876efc49a79814724d2f48bd1a2
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-04-01 15:09:24 +0300

    modified the doctor registration link to route to home when registration is cancelled

--- client/src/components/DoctorReg.js
+++ client/src/components/DoctorReg.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 
 function DoctorReg(){
     return(
@@ -11,7 +12,7 @@ function DoctorReg(){
                 {/* <label>Speciality</label> */}
                 <input type="text" placeholder="enter your speciality here..." className="mb-3 px-2 w-[250px] border-2 border-gray-150" />
                 <input type="submit" value="Register" className="p-1 mb-1 bg-green-500 text-white hover:font-bold w-[250px]"/>
-                <input type="submit" value="Cancel" className="p-1 bg-red-700 text-white hover:font-bold w-[250px]"/>
+                <NavLink path to="/" className="p-1 text-center bg-red-700 text-white hover:font-bold w-[250px]">Cancel</NavLink>
             </form>
         </div>
     )
