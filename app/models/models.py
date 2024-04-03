32a4b001028ad1466dad53059c3858b6774043f4
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-04-03 21:15:26 +0300

    added user, admin, role models

--- app/models/models.py
+++ app/models/models.py
@@ -2,6 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData, ForeignKey
 from sqlalchemy.orm import relationship, validates
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, bcrypt
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -118,5 +122,68 @@ class Appointment(db.Model, SerializerMixin):
             "doctor" : self.doctor.to_dict(visited)
         }
 
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String(250), nullable=False, unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    role = db.relationship('Role', back_populates='user')
+
+    serialize_rules = {
+        "id", "username", "email", 
+        "-role_id","-_password_hash"
+    }
+
+    def __init__(self, username, _password_hash, email, role_id):
+        self.username = username
+        self._password_hash = bcrypt.generate_password_hash(_password_hash).decode('utf-8')
+        self.email = email
+        self.role_id = role_id
     
+    @hybrid_property
+    def password(self):
+        return self._password_hash
+
+    @password.setter
+    def password_hash(self, raw_password):
+        password_hash = bcrypt.generate_password_hash(
+            raw_password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, raw_password):
+        return bcrypt.check_password_hash(
+            self._password_hash, raw_password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+
+class Role(db.Model, SerializerMixin):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    user = db.relationship('User', back_populates='role')
+
+# Admin 
+class Admin(User):
+    __tablename__ = 'admins'
+    id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+
+    def to_dict(self, visited=None):
+        if visited is None:
+            visited = set()
+        if self in visited:
+            return {'id': self.id}  # or any other representation to break the recursion
+        visited.add(self)
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'role': self.role.name
+        }
 
