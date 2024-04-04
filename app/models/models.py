a0dfcb23e6eddd4a94642bdb8b71540ef264be33
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-04-05 00:36:47 +0300

    resolving conflicts

--- app/models/models.py
+++ app/models/models.py
@@ -131,6 +131,7 @@ class User(db.Model, SerializerMixin):
     _password_hash = db.Column(db.String(250), nullable=False, unique=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
     role = db.relationship('Role', back_populates='user')
+    
 
     serialize_rules = {
         "id", "username", "email", 
